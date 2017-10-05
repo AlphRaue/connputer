44144db822f08d1754c7c5c82458ea0b60b0f876
AlphRaue <alph-raue@alph-raue.de>
2017-10-06 00:45:57 +0200

    Neueinstieg Okt.17
    Signed-off-by: AlphRaue <alph-raue@alph-raue.de>

---
+++ js/basis.js
@@ -0,0 +1,6 @@
+/**
+ * Created by AranankA on 26.04.2017 0026.
+ */
+/*$(document).ready(function () {
+    alert("It's cool, man!");
+});*/
---
+++ sites/content.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: AranankA
+ * Date: 26.04.2017 0026
+ * Time: 20:25
+ */
